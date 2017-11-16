6d5d25990a0b04945701a559240ac9ace445042d
Gustice <Jakob.Ger@gmail.com>
2017-11-16 22:25:21 +0100

    Initial Commit
    GUI and LED strip tested successfully
    States are displayed correctly
    Switching and fading works properly
    
    ToDo: Showing of working profiles
    ToDo: On the fly adjustment of profiles
    ToDo: API to control the states from remote @shufflecode - please!

---
+++ PublicStatusIndicator/IndicatorEngine/StatusIndicator.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace PublicStatusIndicator.IndicatorEngine
+{
+    class StatusIndicator
+    {
+        int virtualTemplateLength;
+
+        Color[] VirtualRing;
+
+        Color[] ProcessTemplate;
+        Color[] GoodTemplate;
+        Color[] BadTemplate;
+
+        public StatusIndicator(int targetCnt, int smoothFactor)
+        {
+            virtualTemplateLength = targetCnt * smoothFactor;
+            VirtualRing = new Color[targetCnt];
+
+            ProcessTemplate = new Color[virtualTemplateLength];
+            GoodTemplate = new Color[virtualTemplateLength];
+            BadTemplate = new Color[virtualTemplateLength];
+
+
+            InitCurves(virtualTemplateLength, ref ProcessTemplate, ref GoodTemplate, ref BadTemplate);
+
+            VirPrcsEffect = new RotateEffect(ProcessTemplate, VirtualRing);
+            VirGoodEffect = new PulseEffect(GoodTemplate, VirtualRing);
+            VirBadEffect = new PulseEffect(BadTemplate, VirtualRing);
+        }
+
+        void InitCurves(int length, ref Color[] processTemp, ref Color[] goodTemp, ref Color[] badTemp)
+        {
+            int[] prcs = new int[length];
+            int[] good = new int[length];
+            int[] bad = new int[length];
+            int OffsetBrightnes = Byte.MaxValue / 4;
+
+
+            // Erzeuge einen zentralen Puls für den Process
+            prcs = GenerateGausianPulse(prcs.Length, 10, OffsetBrightnes);
+            for (int i = 0; i < length; i++)
+            {
+                processTemp[i] = Color.FromArgb(0xFF, (byte)prcs[i], (byte)prcs[i], 0x00);
+            }
+
+            // Zeuge einen erkennbaren Puls für den Good-Status 
+            int[] tempG = GenerateGausianPulse(prcs.Length / 2, 3, OffsetBrightnes);
+            for (int i = 0; i < tempG.Length; i++)
+            {
+                good[i] = tempG[i];
+            }
+            for (int i = tempG.Length; i < good.Length; i++)
+            {
+                good[i] = OffsetBrightnes;
+            }
+            for (int i = 0; i < length; i++)
+            {
+                goodTemp[i] = Color.FromArgb(0xFF, 0x00, (byte)good[i], 0x00);
+            }
+
+
+            // Erzuge drei erkennbaren Pulse für den Bad-Status 
+            int[] tempB = GenerateGausianPulse(prcs.Length / 4, 3, OffsetBrightnes);
+            for (int i = 0; i < tempB.Length; i++)
+            {
+                bad[i] = tempB[i];
+                bad[i + tempB.Length] = tempB[i];
+                bad[i + (2 * tempB.Length)] = tempB[i];
+            }
+            for (int i = tempB.Length * 3; i < bad.Length; i++)
+            {
+                bad[i] = OffsetBrightnes;
+            }
+            for (int i = 0; i < length; i++)
+            {
+                badTemp[i] = Color.FromArgb(0xFF, (byte)bad[i], 0x00, 0x00);
+            }
+        }
+
+        public void GetCurvesAsGrayValues(out byte[] processStateProfile, out byte[] goodStateProfile, out byte[] badStateProfile)
+        {
+            processStateProfile = MakeGrayValuesToDataProfile(ProcessTemplate);
+            goodStateProfile = MakeGrayValuesToDataProfile(GoodTemplate);
+            badStateProfile = MakeGrayValuesToDataProfile(BadTemplate);
+        }
+
+        private static byte[] MakeGrayValuesToDataProfile(Color[] colorArray)
+        {
+            byte[] outputArray = new byte[colorArray.Length];
+
+            int[] grayArray = new int[colorArray.Length];
+            int maxValue = 0;
+            for (int i = 0; i < colorArray.Length; i++)
+            {
+                grayArray[i] = colorArray[i].R;
+                grayArray[i] += colorArray[i].G;
+                grayArray[i] += colorArray[i].B;
+                grayArray[i] = (grayArray[i] * colorArray[i].A) / Byte.MaxValue;
+                maxValue = Math.Max(maxValue, grayArray[i]);
+            }
+            // Gleichverteilung auf Byte normieren
+            for (int i = 0; i < grayArray.Length; i++)
+            {
+                outputArray[i] = (byte)(grayArray[i] * Byte.MaxValue / maxValue);
+            }
+
+            return outputArray;
+        }
+
+        private static int[] GenerateGausianPulse(int length, int peakForm, int offset)
+        {
+            int[] tempplate = new int[length];
+            int temporaryMax = 0;
+            int idx = 0;
+            int midLen = tempplate.Length / 2;
+            for (int i = -midLen; i < midLen; i++)
+            {
+                tempplate[idx] = (int)(Math.Exp(-(Math.Pow(peakForm * i / midLen, 2))) * Byte.MaxValue) + offset;
+                temporaryMax = Math.Max(temporaryMax, tempplate[idx]);
+                idx++;
+            }
+            // Gleichverteilung auf Byte normieren
+            for (int i = 0; i < tempplate.Length; i++)
+            {
+                tempplate[i] = tempplate[i] * Byte.MaxValue / temporaryMax;
+            }
+
+            return tempplate;
+        }
+
+
+
+        E_States lastState = E_States.Idle;
+        E_States FadingState;
+
+        int fadingCnt = 0;
+        const int FADING_START = 10;
+
+        public Color[] EffectAccordingToState(E_States currentState)
+        {
+            if (currentState != lastState)
+            {
+                FadingState = lastState;
+                lastState = currentState;
+                fadingCnt = FADING_START;
+                ResetEffekt(currentState);
+            }
+
+            Color[] RingColors1 = new Color[0];
+            Color[] RingColors2 = new Color[0];
+
+            RingColors1 = GenerateNewImage(currentState);
+
+            if (fadingCnt > 0)
+            {
+                fadingCnt--;
+                RingColors2 = GenerateNewImage(FadingState);
+
+                int onfadingCnt = FADING_START - fadingCnt;
+                for (int i = 0; i < VirtualRing.Length; i++)
+                {
+                    VirtualRing[i].R = (byte)(((int)RingColors1[i].R * onfadingCnt + (int)RingColors2[i].R * fadingCnt) / 10);
+                    VirtualRing[i].G = (byte)(((int)RingColors1[i].G * onfadingCnt + (int)RingColors2[i].G * fadingCnt) / 10);
+                    //VirtualRing[i].B = (byte)(((int)RingColors1[i].B * onfadingCnt + (int)RingColors2[i].B * fadingCnt) / 10);
+                    VirtualRing[i].B = 0;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < VirtualRing.Length; i++)
+                {
+                    VirtualRing[i] = RingColors1[i];
+                }
+            }
+            return VirtualRing;
+        }
+
+        private Color[] GenerateNewImage(E_States state)
+        {
+            Color[] colorSeries = new Color[0];
+            switch (state)
+            {
+                case E_States.Idle:
+                    colorSeries = new Color[VirtualRing.Length];
+                    for (int i = 0; i < colorSeries.Length; i++)
+                    {
+                        colorSeries[i] = Color.FromArgb(0xFF, 0x20, 0x20, 0x20);
+                    }
+                    break;
+                case E_States.Progress:
+                    colorSeries = VirPrcsEffect.RotateStep();
+                    break;
+                case E_States.Good:
+                    colorSeries = VirGoodEffect.PulseStep();
+                    break;
+                case E_States.Bad:
+                    colorSeries = VirBadEffect.PulseStep();
+                    break;
+            }
+
+            return colorSeries;
+        }
+
+        private void ResetEffekt(E_States state)
+        {
+            switch (state)
+            {
+                case E_States.Progress:
+                    VirPrcsEffect.ResetIndex();
+                    break;
+                case E_States.Good:
+                    VirGoodEffect.ResetIndex();
+                    break;
+                case E_States.Bad:
+                    VirBadEffect.ResetIndex();
+                    break;
+            }
+        }
+
+        RotateEffect VirPrcsEffect;
+        PulseEffect VirGoodEffect;
+        PulseEffect VirBadEffect;
+
+
+        class RotateEffect
+        {
+            int targetLenght;
+            int currentIdx;
+            int maxIndex;
+            int deltaStep;
+
+            Color[] Source;
+            Color[] Temp;
+
+            public RotateEffect(Color[] source, Color[] target)
+            {
+                currentIdx = 0;
+                targetLenght = target.Length;
+                maxIndex = source.Length;
+                deltaStep = maxIndex / targetLenght;
+
+                Source = source;
+                Temp = new Color[target.Length];
+            }
+
+            public Color[] RotateStep()
+            {
+                int relIdx = currentIdx;
+
+                for (int i = 0; i < targetLenght; i++)
+                {
+                    Temp[i] = Source[relIdx];
+                    relIdx += deltaStep;
+                    relIdx = SaturateIndex(relIdx);
+                }
+
+                currentIdx += deltaStep;
+                currentIdx = SaturateIndex(currentIdx);
+                return Temp;
+            }
+
+            private int SaturateIndex(int idx)
+            {
+                if (idx >= maxIndex)
+                {
+                    idx -= maxIndex;
+                }
+                return idx;
+            }
+
+            public void ResetIndex()
+            {
+                currentIdx = 0;
+            }
+        }
+
+        class PulseEffect
+        {
+            int targetLenght;
+            int currentIdx;
+            int maxIndex;
+            int deltaStep;
+
+            Color[] Source;
+            Color[] Temp;
+
+            public PulseEffect(Color[] source, Color[] target)
+            {
+                currentIdx = 0;
+                targetLenght = target.Length;
+                maxIndex = source.Length;
+                deltaStep = maxIndex / targetLenght;
+
+                Source = source;
+                Temp = new Color[target.Length];
+            }
+
+            public Color[] PulseStep()
+            {
+                for (int i = 0; i < targetLenght; i++)
+                {
+                    Temp[i] = Source[currentIdx];
+                }
+                currentIdx += deltaStep;
+                currentIdx = SaturateIndex(currentIdx);
+                return Temp;
+            }
+
+            private int SaturateIndex(int idx)
+            {
+                if (idx >= maxIndex)
+                {
+                    idx -= maxIndex;
+                }
+                return idx;
+            }
+
+            public void ResetIndex()
+            {
+                currentIdx = 0;
+            }
+        }
+    }
+
+
+    enum E_States
+    {
+        Idle,
+        Progress,
+        Good,
+        Bad,
+    }
+
+}
---
+++ PublicStatusIndicator/MainPage.xaml.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.UI;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using PublicStatusIndicator.IndicatorEngine;
+using Windows.Devices.Spi;
+using Windows.Devices.Enumeration;
+using System.Threading.Tasks;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PublicStatusIndicator
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        #region PropChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+
+        private string _statusOutput = "Status";
+        public string StatusOutput
+        {
+            get { return _statusOutput; }
+            set { _statusOutput = value; NotifyPropertyChanged(); }
+        }
+
+        public ObersableBrushes LEDring { get; } = new ObersableBrushes();
+
+        private SolidColorBrush _centerColor = new SolidColorBrush(Colors.DarkGray);
+        public SolidColorBrush CenterColor
+        {
+            get { return _centerColor; }
+            set
+            {
+                _centerColor = value;
+                NotifyPropertyChanged();
+            }
+        }
+        DispatcherTimer RefreshTimer;
+
+        StatusIndicator VirtualIndicator;
+        StatusIndicator LEDIndicator;
+        LED_APA102 LEDstrip;
+
+        public MainPage()
+        {
+            InitHardware();
+
+            RefreshTimer = new DispatcherTimer();
+            RefreshTimer.Interval = TimeSpan.FromMilliseconds(100);
+            RefreshTimer.Tick += LED_Refresh_Tick;
+
+            this.InitializeComponent();
+            DataContext = this;
+
+            VirtualIndicator = new StatusIndicator(12, 4);
+            LEDIndicator = new StatusIndicator(20, 2);
+        }
+
+        const int VIRTUAL_LEN = 12;
+        const int LEDSTRIP_LEN = 20;
+
+        Color[] VirtualRing = new Color[VIRTUAL_LEN];
+        Color[] RGBring = new Color[LEDSTRIP_LEN];
+
+
+        private async void InitHardware()
+        {
+            await InitSpi();
+            LEDstrip = new LED_APA102(StatusLEDInterface, LEDSTRIP_LEN);
+            LEDstrip.BlankLEDs();
+        }
+
+        SpiDevice StatusLEDInterface;
+        private const int HW_SPI_CS_Line = 0;
+        private const string HW_SPI_LED_Controller = "SPI0";
+
+        private async Task InitSpi()
+        {
+            try
+            {
+                var settings = new SpiConnectionSettings(HW_SPI_CS_Line);
+                settings.ClockFrequency = 4000000;
+                settings.Mode = SpiMode.Mode0; // CLK-Idle ist low, Dataset on Falling Edge, Sample on Rising Edge
+                string spiAqs = SpiDevice.GetDeviceSelector(HW_SPI_LED_Controller);
+                var devicesInfo = await DeviceInformation.FindAllAsync(spiAqs);
+                StatusLEDInterface = await SpiDevice.FromIdAsync(devicesInfo[0].Id, settings);
+            }
+            /* If initialization fails, display the exception and stop running */
+            catch (Exception ex)
+            {
+                throw new Exception("SPI Initialization Failed", ex);
+            }
+        }
+
+
+
+        void LED_Refresh_Tick(object sender, object e)
+        {
+            VirtualRing = VirtualIndicator.EffectAccordingToState(DisplayState);
+            RGBring = LEDIndicator.EffectAccordingToState(DisplayState);
+            LEDring.SetAllVaules(VirtualRing);
+            LEDstrip.SetAllLEDs(RGBring);
+            LEDstrip.UpdateLEDs();
+        }
+
+
+        E_States DisplayState = E_States.Idle;
+
+        Dictionary<E_States, string> StateOutputs = new Dictionary<E_States, string>
+        {
+            {E_States.Idle, "Idle" },
+            {E_States.Progress, "Progress..." },
+            {E_States.Good, "GOOD" },
+            {E_States.Bad, "BAD" },
+        };
+
+
+        private void Button_Action(object sender, RoutedEventArgs e)
+        {
+            Button cmdButton = (Button)sender;
+
+            Color newColor = Colors.DarkGray;
+            switch (cmdButton.Name)
+            {
+                case "Blank":
+                    DisplayState = E_States.Idle;
+                    newColor = Color.FromArgb(0xFF, 0x20, 0x20, 0x20);
+                    break;
+                case "InProgress":
+                    DisplayState = E_States.Progress;
+                    newColor = Color.FromArgb(0xFF, 0x40, 0x40, 0x00);
+
+                    break;
+                case "StateGood":
+                    DisplayState = E_States.Good;
+                    newColor = Color.FromArgb(0xFF, 0x00, 0x80, 0x00);
+                    break;
+                case "StateBad":
+                    DisplayState = E_States.Bad;
+                    newColor = Color.FromArgb(0xFF, 0x80, 0x00, 0x00);
+                    break;
+
+                case "showProfiles":
+                    {
+                        
+
+                    }
+                    
+                    break;
+                default:
+                    break;
+            }
+            StatusOutput = StateOutputs[DisplayState];
+            CenterColor = new SolidColorBrush(newColor);
+
+            RefreshTimer.Start();
+        }
+
+        public class ObersableBrushes : INotifyPropertyChanged
+        {
+            private SolidColorBrush _color1;
+            public SolidColorBrush Color1
+            {
+                get { return _color1; }
+                set { _color1 = value; NotifyPropertyChanged(); }
+            }
+
+            private SolidColorBrush _color2;
+            public SolidColorBrush Color2
+            {
+                get { return _color2; }
+                set { _color2 = value; NotifyPropertyChanged(); }
+            }
+
+            private SolidColorBrush _color3;
+            public SolidColorBrush Color3
+            {
+                get { return _color3; }
+                set { _color3 = value; NotifyPropertyChanged(); }
+            }
+
+            private SolidColorBrush _color4;
+            public SolidColorBrush Color4
+            {
+                get { return _color4; }
+                set { _color4 = value; NotifyPropertyChanged(); }
+            }
+
+            private SolidColorBrush _color5;
+            public SolidColorBrush Color5
+            {
+                get { return _color5; }
+                set { _color5 = value; NotifyPropertyChanged(); }
+            }
+
+            private SolidColorBrush _color6;
+            public SolidColorBrush Color6
+            {
+                get { return _color6; }
+                set { _color6 = value; NotifyPropertyChanged(); }
+            }
+
+            private SolidColorBrush _color7;
+            public SolidColorBrush Color7
+            {
+                get { return _color7; }
+                set { _color7 = value; NotifyPropertyChanged(); }
+            }
+
+            private SolidColorBrush _color8;
+            public SolidColorBrush Color8
+            {
+                get { return _color8; }
+                set { _color8 = value; NotifyPropertyChanged(); }
+            }
+
+            private SolidColorBrush _color9;
+            public SolidColorBrush Color9
+            {
+                get { return _color9; }
+                set { _color9 = value; NotifyPropertyChanged(); }
+            }
+
+            private SolidColorBrush _color10;
+            public SolidColorBrush Color10
+            {
+                get { return _color10; }
+                set { _color10 = value; NotifyPropertyChanged(); }
+            }
+
+            private SolidColorBrush _color11;
+            public SolidColorBrush Color11
+            {
+                get { return _color11; }
+                set { _color11 = value; NotifyPropertyChanged(); }
+            }
+
+            private SolidColorBrush _color12;
+            public SolidColorBrush Color12
+            {
+                get { return _color12; }
+                set { _color12 = value; NotifyPropertyChanged(); }
+            }
+
+            SolidColorBrush[] BrushReferences = new SolidColorBrush[12];
+
+            public ObersableBrushes()
+            {
+                Color1 = new SolidColorBrush(Colors.DarkGray);
+                Color2 = new SolidColorBrush(Colors.DarkGray);
+                Color3 = new SolidColorBrush(Colors.DarkGray);
+                Color4 = new SolidColorBrush(Colors.DarkGray);
+                Color5 = new SolidColorBrush(Colors.DarkGray);
+                Color6 = new SolidColorBrush(Colors.DarkGray);
+                Color7 = new SolidColorBrush(Colors.DarkGray);
+                Color8 = new SolidColorBrush(Colors.DarkGray);
+                Color9 = new SolidColorBrush(Colors.DarkGray);
+                Color10 = new SolidColorBrush(Colors.DarkGray);
+                Color11 = new SolidColorBrush(Colors.DarkGray);
+                Color12 = new SolidColorBrush(Colors.DarkGray);
+            }
+
+            public void SetAllVaules(Color[] newColors)
+            {
+                Color1 = new SolidColorBrush(newColors[0]);
+                Color2 = new SolidColorBrush(newColors[1]);
+                Color3 = new SolidColorBrush(newColors[2]);
+                Color4 = new SolidColorBrush(newColors[3]);
+                Color5 = new SolidColorBrush(newColors[4]);
+                Color6 = new SolidColorBrush(newColors[5]);
+                Color7 = new SolidColorBrush(newColors[6]);
+                Color8 = new SolidColorBrush(newColors[7]);
+                Color9 = new SolidColorBrush(newColors[8]);
+                Color10 = new SolidColorBrush(newColors[9]);
+                Color11 = new SolidColorBrush(newColors[10]);
+                Color12 = new SolidColorBrush(newColors[11]);
+            }
+
+            #region PropChanged
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+            #endregion
+        }
+    }
+}
+
+
+
+
+
+
